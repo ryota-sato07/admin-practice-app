83310d8416a53f40366a1ef44ede6fd80422e05e
ryota-sato07 <satour.0722@gmail.com>
2022-06-15 08:20:26 +0900

    first commit

---
+++ src/app/members/members.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-members',
+  templateUrl: './members.component.html',
+  styleUrls: ['./members.component.css']
+})
+export class MembersComponent implements OnInit {
+
+  member = 'tanaka';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
