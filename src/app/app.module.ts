7d495f14658ba77c310c1d7f93708fcf09e6b422
r.sato <ryouta.sato@ixias.net>
2022-09-23 19:50:43 +0900

    feat: add message service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { BrowserModule }    from '@angular/platform-browser';
 import { AppComponent }     from './app.component';
 import { MembersComponent } from './members/members.component';
 import { MemberDetailComponent } from './member-detail/member-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MembersComponent,
-    MemberDetailComponent
+    MemberDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
