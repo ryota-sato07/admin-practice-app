fa11882140ed905042dad3cb1ee1c18f7639acea
r.sato <ryouta.sato@ixias.net>
2022-09-24 15:30:26 +0900

    feat: add routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MembersComponent }     from './members/members.component';
+
+const routes: Routes = [
+  { path: 'members', component: MembersComponent }
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
